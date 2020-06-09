d272003bae2464f8a7c2e71fafd1b37d89b2aedd
Shane Wilson <65076931+shanewils@users.noreply.github.com>
2020-06-09 15:32:34 +0100

    WTFgit add -A

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 let character = document.getElementById("character");
 let block = document.getElementById("block");
+let coin = document.getElementById("coin");
 
-function jump(){
+const jump = () =>{
     character.classList.add("animate");
     document.getElementById("jump").play();
     setTimeout(function(){
-            character.classList.remove("animate");
-            
-            
+            character.classList.remove("animate");    
     },500);
-}
+}   
 
 const checkDead = setInterval(function(){
+    console.log(window.getComputedStyle(block).getPropertyValue("left"))
     let characterTop = 
     parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = 
     parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>0 && characterTop>225){
+    if(blockLeft<40 && blockLeft>0 && characterTop>225){
+        document.getElementById("theme").pause();
         document.getElementById("dead").play();
         block.style.animation = "none";
         block.style.display = "none";
@@ -24,5 +25,22 @@ const checkDead = setInterval(function(){
     }
 },5);
 
+const checkCoin = setInterval(function(){
+    // console.log(window.getComputedStyle(coin).getPropertyValue("left"))
+    let characterTopCoin = 
+    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let coinLeft = 
+    parseInt(window.getComputedStyle(coin).getPropertyValue("left"));
+    if(coinLeft<40 && coinLeft>=0 && characterTopCoin<225){
+        // document.getElementById("coin").play();
+        // coin.style.animation = "none";
+        // coin.style.display = "none";
+        alert("vmbgfk;dgbm")
+        
+    }
+    // console.log(window.getComputedStyle(character).getPropertyValue("top"))
 
+    
+    
+},5);
 
